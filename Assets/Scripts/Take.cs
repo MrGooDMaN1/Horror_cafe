6f1a7eb4390f36cb0c67090f824f40ec6a995e28
MrGooD(Kirirll) <67270313+MrGooDMaN1@users.noreply.github.com>
2025-11-24 17:29:24 +0300

    Changed the take

--- Assets/Scripts/Take.cs
+++ Assets/Scripts/Take.cs
@@ -7,18 +7,21 @@ public class Take : MonoBehaviour
     [SerializeField] float distance;
     public Transform pos;
     private Rigidbody rb;
+    private TakeOneItem takeOneItem;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        takeOneItem = GameObject.Find("Player").GetComponent<TakeOneItem>();
     }
 
     private void OnMouseDown()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, distance))
+        if (Physics.Raycast(ray, distance) && takeOneItem.take == false)
         {
             rb.isKinematic = true;
+            takeOneItem.take = true;
             rb.MovePosition(pos.position);
         }
     }
@@ -30,6 +33,7 @@ public class Take : MonoBehaviour
             this.gameObject.transform.position = pos.position;
             if (Input.GetKeyDown(KeyCode.G))
             {
+                takeOneItem.take = false;
                 rb.useGravity = true;
                 rb.isKinematic = false;
                 rb.AddForce(Camera.main.transform.forward * 500);
---
+++ Assets/Scripts/TakeOneItem.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeOneItem : MonoBehaviour
+{
+    public bool take;
+}
